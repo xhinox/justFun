c679363b9b553ae0ebd91675feea3c3753a98ad7
Augusto Ferriz (CORP) Coordinador de Tienda en Línea Ext. 2252 <augustof@airesmirage.com>
2018-07-04 17:48:36 -0700

    v7.23

--- js/index.js
+++ js/index.js
@@ -519,7 +519,7 @@ function onDeviceReady(){
 
     document.addEventListener("offline", onOffline, false);
 
-    initPushwoosh();
+    // initPushwoosh();
 
 
     // $('.actCamera').on('click', function(){
@@ -843,102 +843,102 @@ function validar_email( email ) {
 }
 
 // Pushwoosh
-function onPushwooshInitialized(pushNotification) {
-
-	// if you need push token at a later time you can always get it from Pushwoosh plugin
-	// pushNotification.getPushToken(
-	// 	function(token) {
-	// 		console.log('push token: ' + token);
-	// 	}
-	// );
-
-	// and HWID if you want to communicate with Pushwoosh API
-	pushNotification.getPushwooshHWID(
-		function(token) {
-			console.log('Pushwoosh HWID: ' + token);
-		}
-	);
-
-	// settings tags
-	// pushNotification.setTags({
-	// 		tagName: "tagValue",
-	// 		intTagName: 10
-	// 	},
-	// 	function(status) {
-	// 		console.log('setTags success: ' + JSON.stringify(status));
-	// 	},
-	// 	function(status) {
-	// 		console.log('setTags failed');
-	// 	}
-	// );
-
-	// pushNotification.getTags(
-	// 	function(status) {
-	// 		console.log('getTags success: ' + JSON.stringify(status));
-	// 	},
-	// 	function(status) {
-	// 		console.log('getTags failed');
-	// 	}
-	// );
-
-}
-
-function initPushwoosh() {
-	var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
-    //
-	// //set push notifications handler
-	document.addEventListener('push-notification',
-		function(event) {
-			var message = event.notification.message;
-			var userData = event.notification.userdata;
-            // console.log('' + message + '', '' + userData + '');
-            // console.log('' + JSON.stringify(event.notification) + '');
-            //
-			// // document.getElementById("pushMessage").innerHTML = message + "<p>";
-			// // document.getElementById("pushData").innerHTML = JSON.stringify(event.notification) + "<p>";
-            //
-			//dump custom data to the console if it exists
-			if (typeof(userData) != "undefined") {
-				console.log('user data: ' + JSON.stringify(userData));
-			}
-		}
-    );
-    //
-    // document.addEventListener('push-receive',
-    //     function (event) {
-    //         var message = event.notification.message;
-    //         var userData = event.notification.userdata;
-    //         console.log('' + message + '', '' + userData + '');
-    //         console.log('' + JSON.stringify(event.notification) + '');
-    //
-    //         // document.getElementById("pushMessage").innerHTML = message + "<p>";
-    //         // document.getElementById("pushData").innerHTML = JSON.stringify(event.notification) + "<p>";
-    //
-    //         //dump custom data to the console if it exists
-    //         if (typeof (userData) != "undefined") {
-    //             console.log('','user data: ' + JSON.stringify(userData));
-    //         }
-    //     }
-    // );
-    //
-	// //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
-    pushNotification.onDeviceReady({
-        projectid: "430274723087",
-        appid: "3FD66-F50FD",
-        serviceName: ""
-    });
-
-	//register for push notifications
-	pushNotification.registerDevice(
-		function(status) {
-			// document.getElementById("pushToken").innerHTML = status.pushToken + "<p>";
-			onPushwooshInitialized(pushNotification);
-		},
-		function(status) {
-			swal('', JSON.stringify(['failed to register ', status]));
-		}
-	);
-}
+// function onPushwooshInitialized(pushNotification) {
+//
+// 	// if you need push token at a later time you can always get it from Pushwoosh plugin
+// 	// pushNotification.getPushToken(
+// 	// 	function(token) {
+// 	// 		console.log('push token: ' + token);
+// 	// 	}
+// 	// );
+//
+// 	// and HWID if you want to communicate with Pushwoosh API
+// 	pushNotification.getPushwooshHWID(
+// 		function(token) {
+// 			console.log('Pushwoosh HWID: ' + token);
+// 		}
+// 	);
+//
+// 	// settings tags
+// 	// pushNotification.setTags({
+// 	// 		tagName: "tagValue",
+// 	// 		intTagName: 10
+// 	// 	},
+// 	// 	function(status) {
+// 	// 		console.log('setTags success: ' + JSON.stringify(status));
+// 	// 	},
+// 	// 	function(status) {
+// 	// 		console.log('setTags failed');
+// 	// 	}
+// 	// );
+//
+// 	// pushNotification.getTags(
+// 	// 	function(status) {
+// 	// 		console.log('getTags success: ' + JSON.stringify(status));
+// 	// 	},
+// 	// 	function(status) {
+// 	// 		console.log('getTags failed');
+// 	// 	}
+// 	// );
+//
+// }
+//
+// function initPushwoosh() {
+// 	var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+//     //
+// 	// //set push notifications handler
+// 	document.addEventListener('push-notification',
+// 		function(event) {
+// 			var message = event.notification.message;
+// 			var userData = event.notification.userdata;
+//             // console.log('' + message + '', '' + userData + '');
+//             // console.log('' + JSON.stringify(event.notification) + '');
+//             //
+// 			// // document.getElementById("pushMessage").innerHTML = message + "<p>";
+// 			// // document.getElementById("pushData").innerHTML = JSON.stringify(event.notification) + "<p>";
+//             //
+// 			//dump custom data to the console if it exists
+// 			if (typeof(userData) != "undefined") {
+// 				console.log('user data: ' + JSON.stringify(userData));
+// 			}
+// 		}
+//     );
+//     //
+//     // document.addEventListener('push-receive',
+//     //     function (event) {
+//     //         var message = event.notification.message;
+//     //         var userData = event.notification.userdata;
+//     //         console.log('' + message + '', '' + userData + '');
+//     //         console.log('' + JSON.stringify(event.notification) + '');
+//     //
+//     //         // document.getElementById("pushMessage").innerHTML = message + "<p>";
+//     //         // document.getElementById("pushData").innerHTML = JSON.stringify(event.notification) + "<p>";
+//     //
+//     //         //dump custom data to the console if it exists
+//     //         if (typeof (userData) != "undefined") {
+//     //             console.log('','user data: ' + JSON.stringify(userData));
+//     //         }
+//     //     }
+//     // );
+//     //
+// 	// //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+//     pushNotification.onDeviceReady({
+//         projectid: "430274723087",
+//         appid: "3FD66-F50FD",
+//         serviceName: ""
+//     });
+//
+// 	//register for push notifications
+// 	pushNotification.registerDevice(
+// 		function(status) {
+// 			// document.getElementById("pushToken").innerHTML = status.pushToken + "<p>";
+// 			onPushwooshInitialized(pushNotification);
+// 		},
+// 		function(status) {
+// 			swal('', JSON.stringify(['failed to register ', status]));
+// 		}
+// 	);
+// }
 
 function checkEstado(str) {
     switch (str) {
